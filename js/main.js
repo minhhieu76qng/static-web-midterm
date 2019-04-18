88e405530a8b8fd6b63e767c7081b27838500808
HieuDo <minhhieu76qng@gmail.com>
2019-04-18 17:32:16 +0700

    sidebar tu dong truot khi scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var isStick = false;
-window.addEventListener('scroll', function () {
+
+function DisplayHeader() {
     let headerHeight = document.getElementById('header').clientHeight;
 
     if (headerHeight < window.scrollY) {
@@ -18,15 +19,58 @@ window.addEventListener('scroll', function () {
     else {
         if (isStick === true) {
             document.getElementById('header').querySelector('.top-header').style.display = '';
-            
+
             document.getElementById('header').style.cssText = ''
-            
+
             document.getElementById('nav-search').querySelector('.container-fluid').classList.add('container');
             document.getElementById('nav-search').querySelector('.container-fluid').classList.remove('container-fluid');
-            
+
             document.getElementById('nav-search').querySelector('.container .sub-logo').style.display = 'none';
         }
 
         isStick = false;
     }
+}
+
+
+// trước khi chạy thì phải get được vị trí và kích thước của column side bar.
+// khi lăn thì thay đổi vị trí của sidebar-wrapper bằng position absolute và top:
+// khi top + height(sidebar) == height(widget) thì dừng không thêm top nữa.
+function Sidebar() {
+
+    let sidebar = document.querySelector('.widget-sidebar');
+
+    let sidebarTop = sidebar.getBoundingClientRect().top;
+
+    let heightNav = document.getElementById('header').clientHeight;
+
+    let sidebar_wrapper = sidebar.querySelector('.sidebar-wrapper');
+
+    if (sidebarTop - heightNav <= 0) {
+
+        if (Math.floor(-sidebarTop + heightNav) + sidebar_wrapper.clientHeight - (sidebar.clientHeight) < 0) {
+            console.log(-sidebarTop + heightNav);
+
+            sidebar_wrapper.style.cssText = `top: ${Math.floor(-sidebarTop + heightNav)}px`;
+        }
+        else
+        {
+            sidebar_wrapper.style.cssText = 'bottom:0; height:auto; top:auto;'
+        }
+    }
+    else
+    {
+        sidebar_wrapper.style.top = '0';
+    }
+}
+
+window.addEventListener('scroll', function () {
+    DisplayHeader();
+
+    Sidebar();
+
+})
+
+window.addEventListener('load', function () {
+    Sidebar();
 })
